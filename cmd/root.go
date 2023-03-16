35009c3997334c7698329b972eaa5b751ea4c92b
OmegaRogue <omegarogue@omegavoid.codes>
2023-03-16 21:29:40 +0100

    Add copyright notice

--- cmd/root.go
+++ cmd/root.go
@@ -1,19 +1,20 @@
 /*
-Copyright © 2023 OmegaRogue
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright © 2023 omegarogue
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package cmd
 
@@ -60,6 +61,12 @@ to quickly create a Cobra application.`,
 			os.Exit(code)
 		}
 
+	},
+	CompletionOptions: cobra.CompletionOptions{
+		DisableDefaultCmd:   false,
+		DisableNoDescFlag:   false,
+		DisableDescriptions: false,
+		HiddenDefaultCmd:    false,
 	},
 }
 
