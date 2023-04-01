fa11ef522b5ec8ea89781ddf40f53b817c49e4be
OmegaRogue <omegarogue@omegavoid.codes>
2023-04-02 01:11:29 +0200

    :green_heart: Attempt to fix actions workflow
    Fix go generate path

--- internal/gen/docs/docs.go
+++ internal/gen/docs/docs.go
@@ -16,7 +16,7 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-//go:generate go run github.com/OmegaRogue/weylus-desktop/internal/docs ../../../man ../../../docs
+//go:generate go run . ../../../man ../../../docs
 package main
 
 import (
