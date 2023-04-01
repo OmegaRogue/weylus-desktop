d0a5020cc180fd7fef0ba8dad48c86649619bb2b
OmegaRogue <omegarogue@omegavoid.codes>
2023-04-01 15:22:23 +0200

    :page_facing_up: Update license in all files

--- cmd/server.go
+++ cmd/server.go
@@ -1,19 +1,19 @@
 /*
  * Copyright © 2023 omegarogue
- * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package cmd
@@ -23,7 +23,6 @@ import (
 	"net"
 	"os"
 
-	"github.com/OmegaRogue/weylus-desktop/server"
 	"github.com/OmegaRogue/weylus-desktop/web"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
@@ -84,8 +83,8 @@ func NewServerCmd() *cobra.Command {
 				fmt.Println(web.StyleCSS)
 				return
 			}
-			serverLogger := log.With().Str("component", "server").Logger()
-			server.WeylusWebsocket(serverLogger)
+			// TODO serverLogger := log.With().Str("component", "server").Logger()
+			// TODO server.NewWeylusServer()
 			//server.WeylusWeb(serverLogger)
 		},
 	}
